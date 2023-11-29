301b62cedf12d13310dcf2be2272e8da6b9534e5
Océane Lahoual <138216585+Midunighto@users.noreply.github.com>
2023-11-29 11:23:06 +0100

    express02

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -56,6 +56,22 @@ const getMovieById = (req, res) => {
     });
 };
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES ( ?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 /* const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
@@ -71,4 +87,5 @@ const getMovieById = (req, res) => {
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
