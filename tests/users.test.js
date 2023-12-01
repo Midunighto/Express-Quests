b1208927503509a44de76c2e4032c0c2d9e34537
Océane Lahoual <138216585+Midunighto@users.noreply.github.com>
2023-12-01 11:24:22 +0100

    express04

--- tests/users.test.js
+++ tests/users.test.js
@@ -75,7 +75,7 @@ describe("POST /api/users", () => {
       .post("/api/users")
       .send(userWithMissingProps);
 
-    expect(response.status).toEqual(500);
+    expect(response.status).toEqual(422);
   });
 });
 
